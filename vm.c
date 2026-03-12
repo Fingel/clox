27b5fe2cf5e4ee7edc60b1b30070faf66b6a3bbf
Austin Riba <austin@m51.io>
2026-03-12 16:52:38 -0700

    simple arithmetic opcodes

--- vm.c
+++ vm.c
@@ -29,6 +29,12 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+  do { \
+    double b = pop(); \
+    double a = pop(); \
+    push(a op b); \
+  } while (false)
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -48,6 +54,10 @@ static InterpretResult run() {
         push(constant);
         break;
       }
+      case OP_ADD:      BINARY_OP(+); break;
+      case OP_SUBTRACT: BINARY_OP(-); break;
+      case OP_MULTIPLY: BINARY_OP(*); break;
+      case OP_DIVIDE:   BINARY_OP(/); break;
       case OP_NEGATGE:  push(-pop()); break;
       case OP_RETURN: {
         printValue(pop());
@@ -58,6 +68,7 @@ static InterpretResult run() {
   }
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk) {
